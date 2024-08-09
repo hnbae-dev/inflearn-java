7de4996cf5117cb24b63b6fc25870086272ae442
hnbae-dev <hnbae.dev@gmail.com>
2024-08-10 00:12:43 +0900

    자바기본-섹션10-상속

---
+++ java-basic/src/extends1/ex/Album.java
@@ -0,0 +1,18 @@
+package extends1.ex;
+
+public class Album extends Item {
+    private String artist;
+
+    Album() {}
+
+    Album(String name, int price, String artist) {
+        super(name, price);
+        this.artist = artist;
+    }
+
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("- 아티스트: " + artist);
+    }
+}
