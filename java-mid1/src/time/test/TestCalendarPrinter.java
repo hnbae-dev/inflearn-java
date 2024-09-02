f8ab3718e01d5c70256d3bf1b554749f3f0aa539
hnbae-dev <hnbae.dev@gmail.com>
2024-09-02 18:39:02 +0900

    자바중급1-섹션7-날짜와시간

---
+++ java-mid1/src/time/test/TestCalendarPrinter.java
@@ -0,0 +1,39 @@
+package time.test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class TestCalendarPrinter {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("년도를 입력하세요: ");
+        int year = sc.nextInt();
+        sc.nextLine();
+        System.out.print("월을 입력하세요: ");
+        int month = sc.nextInt();
+        sc.nextLine();
+
+        LocalDate firstDate = LocalDate.of(year, month, 1);
+        DayOfWeek firstDay = firstDate.getDayOfWeek();
+
+        int lengthOfMonth = firstDate.lengthOfMonth();
+        int ordinal = firstDay.ordinal();
+        int count = ordinal + 2;
+
+        System.out.printf("%2s %2s %2s %2s %2s %2s %2s\n", "Su", "Mo", "Tu", "We", "Th", "Fr", "Sa");
+        for (int i = 0; i < (count - 1) % 7; i++) {
+            System.out.print("   ");
+        }
+
+        for (int i = 1; i <= lengthOfMonth; i++) {
+            System.out.printf("%2d ", i);
+            if(count % 7 == 0) {
+                System.out.println();
+                count %= 7;
+            }
+            count++;
+        }
+    }
+}
